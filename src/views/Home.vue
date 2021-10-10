dac3e512b4aa0627e1923518c42164c5791cd59a
Mert Doğu <mertdoguonder@gmail.com>
2021-10-10 14:50:35 +0300

    change: bio & stroke color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,12 @@
         <div class="flex items-center md:w-7/12 w-full justify-between">
           <div class="flex h-screen items-center bg-gray-800 p-5 h-50 w-full rounded-xl">
             <img src="../assets/logo.png" class="w-40 h-40 rounded-full border-4 border-gray-700" alt="Logo">
-            <div>
-            <p class="text-2xl">Student,<br/>
+            <div class="ml-2">
+            <p class="md:text-2xl text-gray-100">Student,<br/>
                Developer, Designer.</p>
-               <p>
-                 Hi! My name is Mert. I am a young developer from Turkey. I am coder, editor and designer. I try to develop games in my free time.
+               <p class="text-sm text-gray-400">
+                 Hi! My name is Mert. I am 15-year-old dev from Turkey.
+                 I built web apps using Vue.js, WindiCss and React.js.
                </p>
             </div>
           </div>
