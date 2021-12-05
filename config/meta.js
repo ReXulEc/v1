751eb76cbf4df9d9a7d36fef02717483b9dcce47
Mert Doğu <mertdoguonder@gmail.com>
2021-12-05 14:47:10 +0300

    change: initial scale

--- config/meta.js
+++ config/meta.js
@@ -5,7 +5,7 @@ export default {
   title: 'rexulec. | developer and student',
   meta: [
     { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=5' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     {
       hid: 'description',
       name: 'description',
