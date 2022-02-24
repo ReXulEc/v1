070541c7f74ac672eb6bb730b72bfe71edda6204
Mert Doğu <mertdoguonder@gmail.com>
2022-02-24 23:47:42 +0300

    Update index.tsx

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Header } from "@components/Header";
 import { Navbar } from "@components/Navbar";
-​const index = () => {
+const index = () => {
   return (
     <div className="">
     <Header title="Projects"/>
