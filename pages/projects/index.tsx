47e05fc2abcadcf02262b5f1332385503e6864ab
Mert Doğu <mertdoguonder@gmail.com>
2022-02-24 23:51:54 +0300

    Change: illegal char

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Header } from "@components/Header";
 import { Navbar } from "@components/Navbar";
-const​ ​index​ ​=​ ​function​(​)​{
+const index​ = function​(​)​{
   return (
     <div className="">
     <Header title="Projects"/>
