6a092cff96cd1c5f5a238943829d3bcdb953b1df
Mert Doğu <mertdoguonder@protonmail.com>
2022-02-24 22:51:08 +0300

    add: projects

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,12 +5,12 @@ export const Navbar: FC = () => {
     <div className="flex items-center justify-center sticky p-5 top-0 backdrop-blur-lg">
       <div className="flex md:w-7/12 w-10/12 justify-between">
         <div className="flex items-center">
-          <p className="text-2xl font-bold text-gray-200">
+          <a href="/" className="text-2xl font-bold text-gray-200">
             rexulec.
-          </p>
+          </a>
         </div>
         <div className="flex items-center text-gray-300">
-          <a href="" className="mr-3">
+          <a href="/projects" className="mr-3">
             Projects
           </a>
           <a href="mailto:hi@rexulec.com" className="px-5 p-2 rounded-full bg-[#1f1f1f]">
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -10,14 +10,68 @@ const Home: NextPage = () => {
     <Header title="Projects"/>
         <Navbar />
         <div className="flex justify-center">
-            <div className="grid grid-cols-3 gap-4">
-                <div className="border-2 border-[#101010] rounded-xl p-4 flex text-gray-200">
-                    <div className="border-2 border-[#101010] rounded-xl p-4 flex text-gray-200">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-9/12">
+
+                <div className="border-2 border-[#1f1f1f] rounded-xl text-gray-200">
+                    <div className="border-b-2 border-[#1f1f1f] rounded-t-xl p-4 text-gray-200">
                         <img src="/projects/reponse.png" alt="ReponseLogo" className="w-1/4 rounded-3xl"/>
-                        <p className="ml-2 font-semibold text-xl">Reponse App</p>
+                        <div className="mt-2">
+                            <p className="font-semibold text-xl">Reponse App</p>
+                            <p className="text-gray-600">A new way to chat with your communities and friends.</p>
+                        </div>
                     </div>
-                    <p>gsbshhs</p>
+                    <div className="p-4 py-6">
+                        <div>
+                            <p className="font-bold text-xl">Role</p>
+                            <p className="">Founder & Creator</p>
+                        </div>
+                        <div className="mt-4">
+                            <p className="font-bold text-xl">What</p>
+                            <p className="">I founded Reponse back in 2021 to make comminication more easier than ever!</p>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="border-2 border-[#1f1f1f] rounded-xl text-gray-200">
+                    <div className="border-b-2 border-[#1f1f1f] rounded-t-xl p-4 text-gray-200">
+                        <img src="/projects/reponse.png" alt="ReponseLogo" className="w-1/4 rounded-3xl"/>
+                        <div className="mt-2">
+                            <p className="font-semibold text-xl">Reponse App</p>
+                            <p className="text-gray-600">A new way to chat with your communities and friends.</p>
+                        </div>
+                    </div>
+                    <div className="p-4 py-6">
+                        <div>
+                            <p className="font-bold text-xl">Role</p>
+                            <p className="">Founder & Creator</p>
+                        </div>
+                        <div className="mt-4">
+                            <p className="font-bold text-xl">What</p>
+                            <p className="">I founded Reponse back in 2021 to make comminication more easier than ever!</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="border-2 border-[#1f1f1f] rounded-xl text-gray-200">
+                    <div className="border-b-2 border-[#1f1f1f] rounded-t-xl p-4 text-gray-200">
+                        <img src="/projects/reponse.png" alt="ReponseLogo" className="w-1/4 rounded-3xl"/>
+                        <div className="mt-2">
+                            <p className="font-semibold text-xl">Reponse App</p>
+                            <p className="text-gray-600">A new way to chat with your communities and friends.</p>
+                        </div>
+                    </div>
+                    <div className="p-4 py-6">
+                        <div>
+                            <p className="font-bold text-xl">Role</p>
+                            <p className="">Founder & Creator</p>
+                        </div>
+                        <div className="mt-4">
+                            <p className="font-bold text-xl">What</p>
+                            <p className="">I founded Reponse back in 2021 to make comminication more easier than ever!</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
